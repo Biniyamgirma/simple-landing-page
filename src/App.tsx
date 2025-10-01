1ed2514169dbc99e00646b67f8d986c0b3282c71
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-10-01 10:47:29 +0300

    added all the files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl text-fuchsia-600'>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
